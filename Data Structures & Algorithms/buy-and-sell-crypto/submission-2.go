dce1d8009b5928a8f016866ee3b5c07c735946b4
Azhahes S <46866938+azhahes@users.noreply.github.com>
2026-09-12 12:33:48 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.go
@@ -0,0 +1,13 @@
+func maxProfit(prices []int) int {
+	buy, sell := 0,0
+	maxProfit := 0
+	for sell < len(prices){
+		maxProfit = max(maxProfit, prices[sell]-prices[buy])
+		if prices[buy]>prices[sell]{
+			buy = sell
+		} else {
+			sell++
+		}
+	}
+	return maxProfit
+}
