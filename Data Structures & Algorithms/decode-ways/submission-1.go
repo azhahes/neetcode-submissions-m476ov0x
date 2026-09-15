7458d54caf2efa9e19d697bb3debe1ee59fa187b
Azhahes S <46866938+azhahes@users.noreply.github.com>
2026-09-15 16:13:31 +0530

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.go
@@ -0,0 +1,19 @@
+func numDecodings(s string) int {
+		if len(s) == 0 || s[0] == '0' {
+		return 0
+	}
+	dp := make([]int,len(s)+1)
+	dp[0] = 1
+	dp[1] = 1
+	for i:=2; i < len(dp); i++{
+		if s[i-1] != '0'{
+			dp[i] += dp[i-1]
+		}
+
+		num := (s[i-2]-'0') * 10 + s[i-1] - '0'
+		if num >=10 && num<=26{
+			dp[i] += dp[i-2]
+		}
+	} 
+	return dp[len(s)]
+}
